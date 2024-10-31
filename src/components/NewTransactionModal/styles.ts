6503e6c7e660d951243b9b43695a18930d7051e2
Raphael Tonelli <raphael.tonelli@bhz.jamef.com.br>
2024-10-31 09:08:55 -0300

    Tela inicial finalizada

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,9 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import styled from "styled-components";
+
+export const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  width: 100vh;
+`;
+
+export const Content = styled(Dialog.Content)``;
