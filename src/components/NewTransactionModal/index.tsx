7ff3c3b0ee7fc95f7bba48b4617c7cec7ceae4ea
Raphael Tonelli <raphael.tonelli@bhz.jamef.com.br>
2024-10-31 14:19:16 -0300

    Modal cadastro valores

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { Content, Overlay } from './styles'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 export function NewTransactionModal() {
     return (
@@ -8,14 +9,25 @@ export function NewTransactionModal() {
 
             <Content>
                 <Dialog.Title>Nova Transação</Dialog.Title>
+                <CloseButton>
+                    <X size={24} />
+                </CloseButton>
                 <form action=''>
                     <input type='text' placeholder='Descrição' required />
                     <input type='number' placeholder='Preço' required />
                     <input type='text' placeholder='Categoria' required />
 
+                    <TransactionType>
+                        <TransactionTypeButton variant='income'>
+                            <ArrowCircleUp size={24} />
+                        </TransactionTypeButton>
+                        <TransactionTypeButton variant='outcome'>
+                            <ArrowCircleDown size={24} />
+                        </TransactionTypeButton>
+                    </TransactionType>
+
                     <button type='submit'>Cadastrar</button>
                 </form>
-                <Dialog.Close />
             </Content>
 
         </Dialog.Portal>
